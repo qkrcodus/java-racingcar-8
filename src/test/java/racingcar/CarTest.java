bfc41654f39fc691aa20616a391bad3adc4034d6
qkrcodus <qkrcodus0722@gmail.com>
2025-10-26 21:08:26 +0900

    test(car): 이름 유효성 테스트 추가

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+    @Test
+    @DisplayName("자동차_이름_5자_초과면_IllegalArgumentException을_반환한다")
+    void 자동차_이름_5자_초과면_IllegalArgumentException을_반환한다() {
+        assertThatThrownBy(() -> new Car("abcdef"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+}

f7957e812b04062c2b0609a7fda3f5dab35cc838
qkrcodus <qkrcodus0722@gmail.com>
2025-10-27 10:49:29 +0900

    refactor(car): 매직넘버 상수화 및 생성자 단순화

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,19 +1,21 @@
 package racingcar.model;
 
-public class Car {
+public final class Car {
+    private static final int THRESHOLD_NAME_LENGTH = 5;
+    private static final int THRESHOLD_MOVE = 4;
     private final String name;
     private int score;
 
-    private Car(String name, int score) {
+    private Car(String name) {
         this.name = name;
-        this.score = score;
+        this.score = 0;
     }
 
     public static Car named(String name) {
-        if (name.length() > 5) {
+        if (name.length() > THRESHOLD_NAME_LENGTH) {
             throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
         }
-        return new Car(name, 0);
+        return new Car(name);
     }
 
     public void move(int randomNumber) {
@@ -23,7 +25,7 @@ public class Car {
     }
 
     public static boolean canMove(int randomNumber) {
-        if (randomNumber >= 4) {
+        if (randomNumber >= THRESHOLD_MOVE) {
             return true;
         }
         return false;

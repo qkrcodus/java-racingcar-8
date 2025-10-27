e98f33cde25395c0b4a0f7045dec373d892eab55
qkrcodus <qkrcodus0722@gmail.com>
2025-10-27 20:34:24 +0900

    feat(input): input 유틸 클래스 추가

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,4 +1,7 @@
 package racingcar.controller;
 
 public class RacingController {
+    public void run(){
+
+    }
 }
---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,30 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class InputView {
+    private InputView() {
+    }
+
+    public static List<String> readRacingCarNames() {
+        String inputCarNames = Console.readLine();
+        String[] cars = inputCarNames.split(",");
+        List<String> carNames = new ArrayList<>();
+        for (String car : cars) {
+            carNames.add(car);
+        }
+        return carNames;
+    }
+
+    public static int readNumberOfGames() {
+        String inputNumberOfGames = Console.readLine();
+        int number;
+        number = Integer.parseInt(inputNumberOfGames.trim());
+        if (number <= 0) {
+            throw new IllegalArgumentException("경주 횟수는 0보다 커야 합니다.");
+        }
+        return number;
+    }
+}
